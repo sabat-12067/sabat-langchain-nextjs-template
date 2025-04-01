c28a922792564bd97fa2375101cc986406e8d01d
DESKTOP-4IBC28C\Administrator <greatdev12067@gmail.com>
2025-04-01 06:32:11 -0700

    update

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { useTheme } from "next-themes";
 import { Toaster as Sonner } from "sonner";
-import Link from "next/link";
-
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
